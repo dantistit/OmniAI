6fef6e666f0651d2424adf06266f27307604657b
Breger <daniilbreger13@gmail.com>
2025-01-30 06:46:47 +0400

    Обновление кода перед тестированием Docker

--- server/src/routes/image.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-
-export const imageRouter = Router();
-
-imageRouter.get('/', (_, res) => {
-  res.json({ message: 'Image generation endpoint coming soon' });
-});
