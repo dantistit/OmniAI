69a46a810bce1c9c09b79ec1c15dc361ddfedd8f
Breger <daniilbreger13@gmail.com>
2025-02-02 06:15:56 +0400

    Удалил все файлы

--- server/src/routes/chat.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-
-export const chatRouter = Router();
-
-chatRouter.get('/', (_, res) => {
-  res.json({ message: 'Chat endpoint coming soon' });
-});
--- server/src/routes/subscribe.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-
-export const subscribeRouter = Router();
-
-subscribeRouter.get('/', (_, res) => {
-  res.json({ message: 'Subscription endpoint coming soon' });
-});
